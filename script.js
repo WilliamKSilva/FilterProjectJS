0b02097df3e8c9f03584edbfffc1383b55d00851
William Kelvin <wilz.kelvin12@gmail.com>
2020-12-16 17:26:06 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,27 @@
+ 
+visibilidade("photo-2"); 
+function showCamisetas(id) {
+    var e = document.getElementById("photo-2"); 
+    if(e.style.display == "block") 
+        e.style.display = "none"; 
+    else 
+        e.style.display="block";
+} 
+visibilidade2("photo-3"); 
+function showMoletons(id) {
+    var i = document.getElementById("photo-3"); 
+    if(i.style.display == "block") 
+        i.style.display = "none"; 
+    else 
+        i.style.display="block";
+} 
+visibilidade3("photo-4"); 
+function showCalcas(id) {
+    var o = document.getElementById("photo-4"); 
+    if(o.style.display == "block") 
+        o.style.display = "none"; 
+    else 
+        o.style.display="block";
+} 
+
+
